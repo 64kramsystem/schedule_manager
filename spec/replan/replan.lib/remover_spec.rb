76a9b19bb3a4a280a3910a390b861e6ec2c80ac7
Saverio Miroddi <saverio.pub2@gmail.com>
2025-06-18 10:31:13 +0200

    Remover: Add UT to document bug

--- spec/replan/replan.lib/remover_spec.rb
+++ spec/replan/replan.lib/remover_spec.rb
@@ -18,4 +18,22 @@ describe Remover do
 
     expect { subject.execute(phony_schedule_file, phony_archive_file, content) }.to raise_error("Found todo section into current date (2021-09-20) section!")
   end
+
+  # This is actually a bug in the program.
+  #
+  it "should raise an error if the current date section includes a replan" do
+    # The equal replan lines need to have different indentation in order to trigger the bug.
+    #
+    content = <<~TXT
+        MON 20/SEP/2021
+    - foo (replan 1)
+      - foo (replan 1)
+
+    TXT
+
+    phony_schedule_file = Tempfile.create('schedule')
+    phony_archive_file = Tempfile.create('archive')
+
+    expect { subject.execute(phony_schedule_file, phony_archive_file, content) }.to raise_error('Found `replan` token into current date (2021-09-20) section!')
+  end
 end # describe Remover
